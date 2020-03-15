e9536d4b3af264c67384811e3a1d33d2a74918aa
Priscilla Joly <priscilla.joly@epitech.eu>
2020-03-15 19:50:33 +0100

    table Video migration

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -28,3 +28,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
